9424f9fab851528e9cfd53ca60e973dc30993757
OleksandrVoloshyn <oleksandr.vvn@gmail.com>
2022-02-10 18:35:27 +0200

    class tasks from les 5 done

--- lesson_5/main.js
+++ lesson_5/main.js
@@ -68,3 +68,127 @@
 // ])
 
 // ***************************************************CLASS****************************************************************
+// Всі функції повинні бути описані стрілочним типом!!!!
+//     - створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
+// const getMin = (num1, num2, num3) => {
+//     if (num1 === num2 || num1 === num3 || num2 === num3) {
+//         console.log('мінімум двачисла є одинаковими')
+//     } else if (num1 < num2 && num1 < num3) {
+//         console.log(`min - ${num1}`)
+//     } else if (num2 < num1 && num2 < num3) {
+//         console.log(`min - ${num2}`)
+//     } else if (num3 < num1 && num3 < num2) {
+//         console.log(`min - ${num3}`)
+//     } else {
+//         console.log('error')
+//     }
+// }
+// getMin(2, 3, 1)
+
+// - створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
+// const getMax = (num1, num2, num3) => {
+//     if (num1 === num2 || num1 === num3 || num2 === num3) {
+//         console.log('мінімум двачисла є одинаковими')
+//     } else if (num1 > num2 && num1 > num3) {
+//         console.log(`max - ${num1}`)
+//     } else if (num2 > num1 && num2 > num3) {
+//         console.log(`max - ${num2}`)
+//     } else if (num3 > num1 && num3 > num2) {
+//         console.log(`max - ${num3}`)
+//     } else {
+//         console.log('error')
+//     }
+// }
+// getMax(1, 3, 5)
+
+// - створити функцію яка повертає найбільше число з масиву
+// const getMaxFromArrey = arr => {
+//     let maxNum = arr[0]
+//     for (const candidate of arr) {
+//         if (candidate > maxNum) {
+//             maxNum = candidate
+//         }
+//     }
+//     return maxNum;
+// }
+// console.log(getMaxFromArrey([1, 2, 3, 4, 5, 6, 7, 8, 6, -2]));
+
+// - створити функцію яка повертає найменьше число з масиву
+// const getMinFromArrey = arr => {
+//     let minNum = arr[0]
+//     for (const candidate of arr) {
+//         if (candidate < minNum) {
+//             minNum = candidate
+//         }
+//     }
+//     return minNum;
+// }
+// console.log(getMinFromArrey([1, 2, 3, 4, 5, 6, 7, 8, 6, -2]));
+
+// - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
+// const sumNums = arr =>{
+//     let res = 0
+//     for (const num of arr) {
+//         res += num
+//     }
+//     return res
+// }
+//
+// console.log(sumNums([1, 2, 3, 4, 6, 7]));
+
+// - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
+// const getAvg = arr =>{
+//     let res = 0
+//     for (const num of arr) {
+//         res += num
+//     }
+//     return (res / arr.length)
+// }
+// console.log(getAvg([5, 5, 6,4]));
+
+// - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
+// const getMinLogMax = (...nums) => {
+//     let min = nums[0]
+//     let max = nums[0]
+//     for (const num of nums) {
+//         if (num <= min) {
+//             min = num
+//         } else if (num >= max) {
+//             max = num
+//         }
+//     }
+//     console.log(max)
+//     return min
+// }
+// console.log(getMinLogMax(1, 23, 5, 64, 57, 56, 734, 0));
+
+// - створити функцію яка заповнює масив рандомними числами
+// (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
+// const getRandomArrOfNums = len => {
+//     let arr = [];
+//     for (let i = 0; i < len; i++) {
+//         arr.push(Math.round(Math.random() * 100));
+//     }
+//     console.log(arr);
+// }
+// getRandomArrOfNums(4)
+
+// - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
+// const getRandomArrOfNumsWithLimit = (len, limit) => {
+//     let arr = [];
+//     for (let i = 0; i < len; i++) {
+//         arr.push(Math.round(Math.random()*limit));
+//     }
+//     console.log(arr);
+// }
+// getRandomArrOfNumsWithLimit(4,20)
+
+// - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+// const reverseArr = arr => {
+//     let newArr = []
+//     for (let i = arr.length - 1; i >= 0; i--) {
+//         newArr.push(arr[i])
+//     }
+//     return newArr
+// }
+// console.log(reverseArr([1, 2, 3]));
