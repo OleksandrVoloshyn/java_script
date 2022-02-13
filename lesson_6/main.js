a0643233b25fa928f8522fd6e1111bdbb1d6e25c
OleksandrVoloshyn <oleksandr.vvn@gmail.com>
2022-02-13 23:58:07 +0200

    done home tasks from 6 less

--- lesson_6/main.js
+++ lesson_6/main.js
@@ -1,31 +1,47 @@
 // - Знайти та вивести довижину настипних стрінгових значень
 // 'hello world', 'lorem ipsum', 'javascript is cool'
+// const arr = ['hello world', 'lorem ipsum', 'javascript is cool']
+// arr.forEach(value => console.log(value.length))
+
 // - Перевести до великого регістру наступні стрінгові значення
 // 'hello world', 'lorem ipsum', 'javascript is cool'
+// const arr = ['hello world', 'lorem ipsum', 'javascript is cool']
+// console.log(arr.map(value => value.toUpperCase()));
+
 // - Перевести до нижнього регістру настипні стрінгові значення
 // 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+// const arr = ['HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL']
+// console.log(arr.map(value => value.toLowerCase()));
+
 // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
-//
-//
+// let str = ' dirty string   '
+// console.log(str.trim());
+
 // - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
-//     let str = 'Каждый охотник желает знать';
-// let arr = stringToarray(str);
-// document.writeln(arr); // ['Каждый', 'охотник', 'желает', 'знать']
-//
-//
+// let str = 'Каждый охотник желает знать';
+// const stringToarray = str => str.split(' ')
+// console.log(stringToarray(str));
+
 // - Напишіть функцію delete_characters(str, length), яка повертає підрядок, що складається із зазначеної кількості символів.
-//     let str = 'Каждый охотник желает знать';
-// document.writeln(delete_characters(str, 7)); // Каждый
-//
+// let str = 'Каждый охотник желает знать';
+// const delete_characters = (str, length) => str.substring(0,length)
+// console.log(delete_characters(str, 7));
+
 // - Напишіть функцію insert_dash(str), яка приймає рядок str як аргумент і вставляє тире (-) між словами. При цьому всі символи рядка необхідно перевести у верхній регістр.
-//     let str = "HTML JavaScript PHP";
-// document.writeln(insert_dash(str)); // 'HTML-JAVASCRIPT-PHP'
-//
+// let str = "HTML JavaScript PHP";
+// const insert_dash = str => str.toUpperCase().replaceAll(' ', '-')
+// console.log(insert_dash(str));
+
 // - Напишіть функцію, яка приймає рядок як аргумент і перетворює регістр першого символу рядка з нижнього регістру у верхній.
-//
-//
-//
+// const foo = str => str.charAt(0).toLowerCase() + str.slice(1);
+// console.log(foo('Some text'))
+
 // - Напишіть функцію capitalize(str), яка повертає рядок, у якому кожне слово починається з великої літери.
+// const capitalize = str => {
+//     const arr = str.split(' ')
+//     return arr.map(value => value.charAt(0).toUpperCase() + value.slice(1)).join(' ')
+// }
+// console.log(capitalize('Some txt some txt'));
 
 
 // ***************************************************Class******************************************************
