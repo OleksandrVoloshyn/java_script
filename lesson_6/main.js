3596c4ce43d730f49e831f59b5dbbc4f33ab9d7d
OleksandrVoloshyn <oleksandr.vvn@gmail.com>
2022-02-14 00:38:55 +0200

    done class tasks from 6 less

--- lesson_6/main.js
+++ lesson_6/main.js
@@ -45,27 +45,59 @@
 
 
 // ***************************************************Class******************************************************
-// - Дано список імен.
-//     let n1 = 'Harry..Potter'
+// // - Дано список імен.
+// let n1 = 'Harry..Potter'
 // let n2 = 'Ron---Whisley'
 // let n3 = 'Hermione__Granger'
-// Написати функцію, яка приймає будь яке не валідне імя, та нормалізує його в наступнйи вигляд
-// let n1 = 'Harry Potter'
-// let n2 = 'Ron Whisley'
-// let n3 = 'Hermione Granger'
-//
-//
+// // Написати функцію, яка приймає будь яке не валідне імя, та нормалізує його в наступнйи вигляд
+// // let n1 = 'Harry Potter'
+// // let n2 = 'Ron Whisley'
+// // let n3 = 'Hermione Granger'
+// const normalizeStr = str => {
+//     return str
+//         .replaceAll('.', ' ')
+//         .replaceAll('-', ' ')
+//         .replaceAll('_', ' ')
+//         .split(' ')
+//         .filter(value => value)
+//         .join(' ')
+// }
+// console.log(normalizeStr(n2));
+
 //     - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.
+// const get100NumsArr = () => {
+//     let arr = []
+//     for (let i = 0; i < 100; i++) {
+//         arr.push(Math.round(Math.random() * 100))
+//     }
+//     return arr
+// }
+// let arr = get100NumsArr();
+
 // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. Відсортувати його за допомоги sort
+// arr.sort((a, b) => a - b)
+// console.log(arr);
+
 // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. відфільтрувати  його за допомоги filter, залишивши тільки парні числа
+// let newArr = arr.filter(value => value && !(value % 2))
+// console.log(newArr);
+
 // - створити масив рандомних цілих числових значень (або згенерувати, за допомоги попередньої функції) . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
+// let newArr = arr.map(value => value.toString())
+// console.log(newArr);
+
 // - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
 //     let nums = [11,21,3];
 // sortNums('ascending') // [3,11,21]
 // sortNums('descending') // [21,11,3]
-//
-//
-// - є масив
+
+// const sortNums = (arr, direction) => {
+//     return arr.sort((a, b) => direction === 'ascending' ? a - b : b - a)
+// }
+// console.log(sortNums(arr, 'ascending'));
+
+
+// // - є масив
 // let coursesAndDurationArray = [
 //     {title: 'JavaScript Complex', monthDuration: 5},
 //     {title: 'Java Complex', monthDuration: 6},
@@ -74,8 +106,11 @@
 //     {title: 'FullStack', monthDuration: 7},
 //     {title: 'Frontend', monthDuration: 4}
 // ];
-// -- відсортувати його за спаданням за monthDuration
-// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+// // -- відсортувати його за спаданням за monthDuration
+// // console.log(coursesAndDurationArray.sort((a, b)  => b['monthDuration'] - a['monthDuration']));
+//
+// // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+// console.log(coursesAndDurationArray.filter(value => value.monthDuration > 5))
 
 // *************************************************************Aditional***************************************
 // - Напишите функцию cutString(str, n), которая делит строку на подстроки, состоящие из n символов.
