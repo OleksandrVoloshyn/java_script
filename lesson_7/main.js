23b3cf2720231ea873ac760167df702d6af82c46
OleksandrVoloshyn <oleksandr.vvn@gmail.com>
2022-02-17 22:19:27 +0200

    done additional task from txt file

--- lesson_7/main.js
+++ lesson_7/main.js
@@ -188,64 +188,147 @@
 
 // ***************************************************Ad****************************************************
 //
-// - Створити клас або функцію конструктор, за допомоги якої можна створювати об'єкти наступного вигляду.
-// Конструктор повинен приймати значення для кожної властивості, в т.ч і для властивостей внутрішніх об'єктів
+// // - Створити клас або функцію конструктор, за допомоги якої можна створювати об'єкти наступного вигляду.
+// // Конструктор повинен приймати значення для кожної властивості, в т.ч і для властивостей внутрішніх об'єктів
+// //
+// // {
+// //     id: 1,
+// //     name: 'Leanne Graham',
+// //     username: 'Bret',
+// //     email: 'Sincere@april.biz',
+// //     address: {
+// //         street: 'Kulas Light',
+// //         suite: 'Apt. 556',
+// //         city: 'Gwenborough',
+// //         zipcode: '92998-3874',
+// //         geo: {
+// //             lat: '-37.3159',
+// //             lng: '81.1496'
+// //         }
+// //     },
+// //     phone: '1-770-736-8031 x56442',
+// //     website: 'hildegard.org',
+// //     company: {
+// //         name: 'Romaguera-Crona',
+// //         catchPhrase: 'Multi-layered client-server neural-net',
+// //         bs: 'harness real-time e-markets'
+// //     }
+// // }
 //
-// {
-//     id: 1,
-//         name: 'Leanne Graham',
-//     username: 'Bret',
-//     email: 'Sincere@april.biz',
-//     address: {
-//     street: 'Kulas Light',
-//         suite: 'Apt. 556',
-//         city: 'Gwenborough',
-//         zipcode: '92998-3874',
-//         geo: {
-//         lat: '-37.3159',
-//             lng: '81.1496'
+// class Customer {
+//     constructor(id, name, username, email, address, phone, website, company) {
+//         this.id = id;
+//         this.name = name;
+//         this.username = username;
+//         this.email = email;
+//         this.address = address;
+//         this.phone = phone;
+//         this.website = website;
+//         this.company = company
 //     }
-// },
-//     phone: '1-770-736-8031 x56442',
-//         website: 'hildegard.org',
-//     company: {
-//     name: 'Romaguera-Crona',
-//         catchPhrase: 'Multi-layered client-server neural-net',
-//         bs: 'harness real-time e-markets'
-// }
 // }
 //
-//
-// -  Створити функцію конструктор / клас  який описує об'єкт тегу
-// Поля :
-//     -назва тегу ()
-// - опис його дій
-// - масив з атрибутами (2-3 атрибути максимум)
-// Кожен атрибут описати як окремий який буде містити
-// -назву атрибуту
-// -опис дії атрибуту
-// інформацію брати з htmlbook.ru
-//
-// Таким чином описати теги
-// -a
-// -div
-// -h1
-// -span
-// -input
-// -form
-// -option
-// -select
-// Приклад результуючого об'єкту
-// {
-//     titleOfTag: 'area',
-//         action: `Каждый элемент <area> определяет активные области изображения, которые являются ссылками...`,
-//     attrs: [
-//     {titleOfAttr: 'accesskey', actionOfAttr: 'Переход к области с помощью комбинации клавиш'},
-//     {/*some props and values*/},
-//     {/*...*/},
-//     {/*...*/},
-// ]
-//
+// class Address {
+//     constructor(street, suite, city, zipcode, lat, lng) {
+//         this.street = street;
+//         this.suite = suite;
+//         this.city = city;
+//         this.zipcode = zipcode;
+//         this.geo = {lat, lng}
+//     }
 // }
-// ***************************************************closures****************************************************
+//
+// class Company {
+//     constructor(name, catchPhrase, bs) {
+//         this.name = name;
+//         this.catchPhrase = catchPhrase;
+//         this.bs = bs;
+//     }
+// }
+//
+// let user = new Customer(
+//     1, 'Sasha', 'Sanya', 'mail@gmail.com',
+//     new Address('myStreet', '1488', 'lviv', '123123', '-37.234', '82.2312'), '+3801287468',
+//     'hildegard.org', new Company('okten', 'client-server ', 'harness real-time')
+// )
+// console.log(user)
+
+
+// // -  Створити функцію конструктор / клас  який описує об'єкт тегу
+// // Поля :
+// //     -назва тегу ()
+// // - опис його дій
+// // - масив з атрибутами (2-3 атрибути максимум)
+// // Кожен атрибут описати як окремий який буде містити
+// // -назву атрибуту
+// // -опис дії атрибуту
+// // інформацію брати з htmlbook.ru
+// //
+// // Таким чином описати теги
+// // -a
+// // -div
+// // -h1
+// // -span
+// // -input
+// // -form
+// // -option
+// // -select
+// // Приклад результуючого об'єкту
+// // {
+// //     titleOfTag: 'area',
+// //         action: `Каждый элемент <area> определяет активные области изображения, которые являются ссылками...`,
+// //     attrs: [
+// //     {titleOfAttr: 'accesskey', actionOfAttr: 'Переход к области с помощью комбинации клавиш'},
+// //     {/*some props and values*/},
+// //     {/*...*/},
+// //     {/*...*/},
+// // ]
+// //
+// // }
+//
+// class Tag {
+//     constructor(titleOfTag, action, attr) {
+//         this.titleOfTag = titleOfTag;
+//         this.action = action;
+//         this.attr = attr
+//     }
+// }
+//
+// class Attribute {
+//     constructor(nameOfAttribute, action) {
+//         this.nameOfAttribute = nameOfAttribute;
+//         this.action = action;
+//     }
+// }
+//
+// const a = new Tag('a', 'предназначен для создания ссылок', [
+//     new Attribute('href', 'Задает адрес документа, на который следует перейти.'),
+//     new Attribute('title', 'Добавляет всплывающую подсказку к тексту ссылки.')
+// ])
+// const div = new Tag('div', 'является блочным элементом ', [
+//     new Attribute('align', 'Задает выравнивание содержимого тега '),
+//     new Attribute('title', 'Добавляет всплывающую подсказку к содержимому.')
+// ])
+// const h1 = new Tag('h1', 'наиболее важный заголовок', [
+//     new Attribute('align', 'Определяет выравнивание заголовка.')
+// ])
+// const span = new Tag('span', 'предназначен для определения строчных элементов документа', [
+//     new Attribute('class', 'позволяет связать тег со стилевым оформлением')
+// ])
+// const input = new Tag('input', 'позволяет создавать разные элементы интерфейса', [
+//     new Attribute('type', 'Сообщает браузеру, к какому типу относится элемент формы.')
+// ])
+// const form = new Tag('form', 'устанавливает форму на веб-странице', [
+//     new Attribute('action', 'Адрес программы или документа, который обрабатывает данные формы.')
+// ])
+// const option = new Tag('option', 'определяет отдельные пункты списка', [
+//     new Attribute('value', 'Значение пункта списка'),
+//     new Attribute('selected', 'Заранее устанавливает определенный пункт списка выделенным')
+// ])
+// const select = new Tag('select', 'позволяет создать элемент интерфейса в виде раскрывающегося списка', [
+//     new Attribute('form', 'Связывает список с формой.'),
+//     new Attribute('name', 'Имя элемента для отправки на сервер или обращения через скрипты.')
+// ])
+
+// // ***************************************************closures****************************************************
 // https://github.com/GrayHead/js_demos/blob/master/js/preview/lesson7/lesson7_tasks/additional/%D1%81losures.pdf
