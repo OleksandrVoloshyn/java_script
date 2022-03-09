3b1b1a70c3fdf0e22218934566f15957794cea9f
OleksandrVoloshyn <oleksandr.vvn@gmail.com>
2022-03-09 21:33:14 +0200

    done home tasks from less 11

--- lesson_11/main.js
+++ lesson_11/main.js
@@ -1,8 +1,27 @@
 // -створити форму з інпутами для name та age.
 //     При відправці форми записати об'єкт в localstorage
+// let myForm = document.forms.main;
+// myForm.addEventListener('submit', e => {
+//     e.preventDefault()
+//     console.log(myForm.age.value)
+// localStorage.setItem('name', myForm.name.value)
+// localStorage.setItem('age', myForm.age.value)
 //
+//     localStorage.setItem('user', JSON.stringify({'name': myForm.name.value, 'age': myForm.age.value}))
+// })
+
 // -створити форму з інпутами для model,type та volume автівки.
 //     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+// let carForm = document.forms.car;
+// carForm.addEventListener('submit', e => {
+//     e.preventDefault()
+//     localStorage.setItem('car', JSON.stringify({
+//         'model': carForm.model.value,
+//         'type': carForm.type.value,
+//         'volume': carForm.volume.value
+//     }))
+// })
+
 // *************************************************Class**********************************************
 // є масив -
 // let users = [
