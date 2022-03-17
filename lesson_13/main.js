e5ae8a0f05d1c11518bc7eb1212abf06c1e225eb
OleksandrVoloshyn <oleksandr.vvn@gmail.com>
2022-03-17 22:55:39 +0200

    less 13 done my day callback hall

---
+++ lesson_13/main.js
@@ -0,0 +1,31 @@
+// const day = () => {
+//     setTimeout(() => {
+//         console.log('Проснувся')
+//         setTimeout(() => {
+//             console.log('Душ, всі діла')
+//             setTimeout(() => {
+//                 console.log('Гоу ту ворк')
+//                 setTimeout(() => {
+//                     console.log('Воркаю')
+//                     setTimeout(() => {
+//                         console.log('Їду додом')
+//                         setTimeout(() => {
+//                             console.log('Доробити дз')
+//                             setTimeout(() => {
+//                                 console.log('лекція')
+//                                 setTimeout(() => {
+//                                     console.log('Домашка')
+//                                     setTimeout(() => {
+//                                         console.log('Спати')
+//                                     }, 500)
+//                                 }, 1000)
+//                             }, 1500)
+//                         }, 1000)
+//                     }, 500)
+//                 }, 2000)
+//             }, 300)
+//         }, 500)
+//     }, 1000)
+// }
+// day()
+
