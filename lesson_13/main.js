63a797890518fcf24909c84368d27de792ecf2e6
OleksandrVoloshyn <oleksandr.vvn@gmail.com>
2022-03-18 15:11:46 +0200

    done task from less 13 with promise

--- lesson_13/main.js
+++ lesson_13/main.js
@@ -1,78 +1,152 @@
-const wakeUp = (isTired, cb) => {
-    console.log('Проснувся')
-    setTimeout(() => {
-        if (isTired) {
-            cb('Сидиш дома нема сили', null)
-        } else {
-            cb(null, 'Ранкова рутина')
-        }
-    }, 500)
-}
+// const wakeUp = (isTired, cb) => {
+//     console.log('Проснувся')
+//     setTimeout(() => {
+//         if (isTired) {
+//             cb('Сидиш дома нема сили', null)
+//         } else {
+//             cb(null, 'Ранкова рутина')
+//         }
+//     }, 500)
+// }
+//
+// const goToWork = (hasJob, cb) => {
+//     setTimeout(() => {
+//         if (hasJob) {
+//             cb(null, 'Воркаю')
+//         } else {
+//             cb('Немає роботи вмер з голоду')
+//         }
+//     }, 1000)
+// }
+//
+// const getSalary = (isEndOfMonth, cb) => {
+//     console.log('Кінець робочого дня')
+//     setTimeout(() => {
+//         if (isEndOfMonth) {
+//             cb(null, 'Получаю зп')
+//         } else {
+//             cb('Нема бабок, йду додому')
+//         }
+//     }, 500)
+// }
+//
+// const hangOut = (hasFight, cb) => {
+//     console.log('Тусуюсь')
+//     setTimeout(() => {
+//         if (hasFight) {
+//             console.log('Намічажться замєс')
+//             cb('Получив тягла йду додому спати')
+//         } else {
+//             cb(null, 'З довольним лицей йду додому спати')
+//         }
+//     }, 1000)
+// }
+//
+// wakeUp(false, (err, data) => {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log(data)
+//
+//         goToWork(true, (err, data) => {
+//             if (err) {
+//                 console.log(err)
+//             } else {
+//                 console.log(data)
+//
+//                 getSalary(true, (err, data) => {
+//                     if (err) {
+//                         console.log(err)
+//                     } else {
+//                         console.log('Ураааа')
+//                         console.log(data)
+//
+//                         hangOut(false, (err, data) => {
+//                             if (err) {
+//                                 console.log(err)
+//                                 console.log('Ліг спати')
+//                             } else {
+//                                 console.log(data)
+//                                 console.log('Ліг довольний спати')
+//                             }
+//                         })
+//                     }
+//                 })
+//             }
+//         })
+//     }
+// })
 
-const goToWork = (hasJob, cb) => {
-    setTimeout(() => {
-        if (hasJob) {
-            cb(null, 'Воркаю')
-        } else {
-            cb('Немає роботи вмер з голоду')
-        }
-    }, 1000)
-}
+// ******************************************************Promise***********************************************************
 
-const getSalary = (isEndOfMonth, cb) => {
-    console.log('Кінець робочого дня')
-    setTimeout(() => {
-        if (isEndOfMonth) {
-            cb(null, 'Получаю зп')
-        } else {
-            cb('Нема бабок, йду додому')
-        }
-    }, 500)
-}
-
-const hangOut = (hasFight, cb) => {
-    console.log('Тусуюсь')
-    setTimeout(() => {
-        if (hasFight) {
-            console.log('Намічажться замєс')
-            cb('Получив тягла йду додому спати')
-        } else {
-            cb(null, 'З довольним лицей йду додому спати')
-        }
-    }, 1000)
-}
-
-wakeUp(false, (err, data) => {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log(data)
-
-        goToWork(true, (err, data) => {
-            if (err) {
-                console.log(err)
+const wakeUp = (isTired) => {
+    return new Promise((resolve, reject) => {
+        console.log('Проснувся')
+        setTimeout(() => {
+            if (isTired) {
+                reject('Нема сили сиди вдома')
             } else {
-                console.log(data)
-
-                getSalary(true, (err, data) => {
-                    if (err) {
-                        console.log(err)
-                    } else {
-                        console.log('Ураааа')
-                        console.log(data)
-
-                        hangOut(false, (err, data) => {
-                            if (err) {
-                                console.log(err)
-                                console.log('Ліг спати')
-                            } else {
-                                console.log(data)
-                                console.log('Ліг довольний спати')
-                            }
-                        })
-                    }
-                })
+                resolve('Ранкова рутина')
             }
-        })
-    }
-})
+        }, 500)
+    })
+}
+
+const goToWork = (hasJob) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (hasJob) {
+                resolve('Заробляю касу')
+            } else {
+                reject('Немає шо їсти(( RIP')
+            }
+        }, 1000)
+    })
+}
+
+const getSalary = (isEndOfMonth) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (isEndOfMonth) {
+                resolve('Отримав зп')
+            } else {
+                reject('Немає грошей')
+            }
+        }, 1000)
+    })
+}
+
+const hangOut = (hasFight) => {
+    return new Promise((resolve, reject) => {
+        console.log('тусуюсь..')
+        setTimeout(() => {
+            if (hasFight) {
+                console.log('Намічається драка')
+                reject('Получив тягла')
+            } else {
+                resolve('Іду додом')
+            }
+        }, 1000)
+    })
+
+}
+
+wakeUp(false)
+    .then(value => {
+        console.log(value)
+        return goToWork(true)
+    })
+    .then(value => {
+        console.log('Іду на роботу')
+        console.log(value)
+        return getSalary(true)
+    })
+    .then(value => {
+        console.log(value)
+        return hangOut(false)
+    })
+    .then(value => {
+        console.log(value)
+        console.log('Лягаю спати')
+    })
+    .catch(reason => console.error(reason))
